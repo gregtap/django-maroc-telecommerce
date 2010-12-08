1af9a158033dd4dfbe7f45d58c9e8e6906216b73
coulix <coulix@gmail.com>
2010-12-08 17:33:11 +0100

    update settings

--- maroc_telecommerce/settings.py
+++ maroc_telecommerce/settings.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 
 GATEWAY_URL = ""
-GATEWAY_TEST_URL = u"http://shopping.maroctelecommerce.com/test/gateway/paiement.asp"
+#GATEWAY_TEST_URL = u"http://shopping.maroctelecommerce.com/test/gateway/paiement.asp"
+
+GATEWAY_TEST_URL = settings.MT_URL
 
 STORE_ID = settings.MT_STORE_ID
 SECRET = settings.MT_SECRET
